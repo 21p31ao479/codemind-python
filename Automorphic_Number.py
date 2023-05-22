1ae4f593c75b813c13995d56451ecf47f1b069eb
21p31ao479 <99890912+21p31ao479@users.noreply.github.com>
2023-05-22 16:05:12 +0530

    n=int(input()) a=10 sq=0 i=0 while True:     if n<a:         sq=n**2         d=sq%a         if n==d:             print("Automorphic Number")             break         else:             print("Not an Automorphic Number")             break     a=a*10     i+=1

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,9 +1,16 @@
 n=int(input())
-n_of_digits=len(str(n))
-square=n**2
-last_digits=square%pow(10,n_of_digits)
-if last_digits==n:
-    print("Automorphic Number")
-else:
-    print("Not an Automorphic Number")
-    
+a=10
+sq=0
+i=0
+while True:
+    if n<a:
+        sq=n**2
+        d=sq%a
+        if n==d:
+            print("Automorphic Number")
+            break
+        else:
+            print("Not an Automorphic Number")
+            break
+    a=a*10
+    i+=1
